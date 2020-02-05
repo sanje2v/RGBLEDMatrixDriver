d814b31dda0a0002bc8a69a1932709ce659eed02
Sanjeev <swtbase@gmail.com>
2020-02-05 12:27:39 +1100

    Using the same buffer to read and write from. On second thought, there might be synchronization issue here. This needs testing. Will also write a version with two buffer each for read and write.

--- RGBLEDMatrixDriver/controller/Settings.h
+++ RGBLEDMatrixDriver/controller/Settings.h
@@ -1,3 +1,4 @@
+
 // SETTINGS
 //#define PRINT_MILLIS_PER_FRAME      // NOTE: Uncomment this line to print milliseconds that a frame is shown to serial
 
@@ -11,8 +12,8 @@
 #define NUM_COLORS_PER_ROW_DOT                3
 #define ONE_MATRIX_FRAME_SIZE                 (NUM_ROWS_PER_MATRIX * NUM_COLORS_PER_ROW_DOT)
 #define ONE_FRAME_SIZE                        (NUM_LED_MATRICES * ONE_MATRIX_FRAME_SIZE)
-#define MAX_FRAME_BUFFER_SIZE                 (12 * ONE_FRAME_SIZE)   // NOTE: For 4 matrices, this allows for 12 frames
-#define MAX_FRAMES                            uint8_t(MAX_FRAME_BUFFER_SIZE / ONE_FRAME_SIZE)
-#define NUM_REDRAW_EACH_FRAME                 118                     // NOTE: This value allows us to set milliseconds to show per frame
-#define SLAVE_SELECT_PINS								      { 10, 11, 12, 13 }      // CAUTION: Make sure the number of pins match 'NUM_LED_MATRICES'
-#define _SS_MAX_RX_BUFF                       128                     // CAUTION: Needs to be able to hold one frame and should be a power of 2
+#define TOTAL_FRAMES                          12
+#define TOTAL_FRAME_BUFFER_SIZE               (TOTAL_FRAMES * ONE_FRAME_SIZE)   // NOTE: For 4 matrices, this allows for 12 frames
+#define NUM_REDRAW_EACH_FRAME                 118                 // NOTE: This value allows us to set milliseconds to show per frame
+#define _LED_MATRIX_SELECT_PINS     				  { 10, 11, 12, 13 }  // CAUTION: Make sure the number of pins match 'NUM_LED_MATRICES'
+#define _SS_MAX_RX_BUFF                       128                 // CAUTION: Needs to be able to hold one frame and should be a power of 2
--- RGBLEDMatrixDriver/controller/controller.ino
+++ RGBLEDMatrixDriver/controller/controller.ino
@@ -1,7 +1,9 @@
 /*
  * This code is to be executed in an Arduino. It was tested in Arduino UNO and 
  * intended to drive EP-0075 LED matrices using SPI.
+ * 
  * Written by: Sanjeev Sharma. Copyright 2020.
+ * https://sanje2v.wordpress.com/
  */
 
 #include "Settings.h"
@@ -10,29 +12,20 @@
 #include <SPI.h>
 
 
-// Constants
-#define BITS_PER_BYTE                 8
-#define SYNC_BITS_PER_SERIAL_FRAME    2   // 1 start and 1 stop bit
-#define SERIAL_FRAME_SIZE             (BITS_PER_BYTE + SYNC_BITS_PER_SERIAL_FRAME)
-#define MILLIS_PER_SECOND             1000
-#define MILLIS_REQUIRED_PER_FRAME     ((SERIAL_FRAME_SIZE * ONE_FRAME_SIZE * uint32_t(MILLIS_PER_SECOND))/SERIAL_SPEED_BPS)
-
-static const int LEDMATRIX_SELECT_PINS[NUM_LED_MATRICES] = SLAVE_SELECT_PINS;
+// Static constants
+static const uint8_t LED_MATRIX_SELECT_PINS[NUM_LED_MATRICES] = _LED_MATRIX_SELECT_PINS;
 
 // Global allocation
 static char g_pStringBuffer[128];
-static byte g_pFrameBuffer[MAX_FRAME_BUFFER_SIZE];
-static int g_FrameBufferSize;
+static byte g_pFrameBuffer[TOTAL_FRAME_BUFFER_SIZE];
 static uint8_t g_CurrentFrameIndex;
 static SoftwareSerial SSerial(2, 3); // pins in order of (RX, TX)
 
 
 void fillFrameBufferWithDefaultPattern()
 {
-  const uint8_t NUM_DEFAULT_FRAMES = 12;  // CAUTION: This value should not exceed 'MAX_FRAMES'
-
   byte rowStates[ONE_FRAME_SIZE];
-  for (uint8_t i = 0; i < NUM_DEFAULT_FRAMES; ++i)
+  for (uint8_t i = 0; i < TOTAL_FRAMES; ++i)
   {
     /*if (i % 3 == 0)
     {
@@ -71,7 +64,7 @@ void fillFrameBufferWithDefaultPattern()
         for (uint8_t l = 0; l < NUM_COLORS_PER_ROW_DOT; ++l)
         {
           byte rowState;
-          if (i < (NUM_DEFAULT_FRAMES/2))
+          if (i < (TOTAL_FRAMES/2))
           {
             rowState = (l == 1 ? 0x00 : 0xFF);
           }
@@ -84,9 +77,6 @@ void fillFrameBufferWithDefaultPattern()
       }
     }
   }
-  
-  // Set size of frame buffer for default pattern
-  g_FrameBufferSize = NUM_DEFAULT_FRAMES * ONE_FRAME_SIZE;
   
   // Point to first frame
   g_CurrentFrameIndex = 0;
@@ -111,8 +101,8 @@ void setup()
   // Configure SPI Slave Select pins as output pins and deselected slaves in SPI
   for (uint8_t i = 0; i < NUM_LED_MATRICES; ++i)
   {
-    pinMode(LEDMATRIX_SELECT_PINS[i], OUTPUT);
-    digitalWrite(LEDMATRIX_SELECT_PINS[i], HIGH);
+    pinMode(LED_MATRIX_SELECT_PINS[i], OUTPUT);
+    digitalWrite(LED_MATRIX_SELECT_PINS[i], HIGH);
   }
   
   // Fill default pattern for frame buffer
@@ -126,15 +116,15 @@ void loop()
 {
   // If this is the last frame, notify host
   // NOTE: This hint can allow the host to send next set of frames.
-  if ((g_CurrentFrameIndex + 1) == uint8_t(g_FrameBufferSize / ONE_FRAME_SIZE))
+  if ((g_CurrentFrameIndex + 1) == TOTAL_FRAMES)
     SSerial.println(F("COMPLETED"));
   
   // Draw current frame
-  // NOTE: We redraw each frame multiple times as we cannot use delay (as display state don't hold)
   #ifdef PRINT_MILLIS_PER_FRAME
   unsigned long start_time = millis();
   #endif
-  
+
+  // NOTE: We redraw each frame multiple times as we cannot use delay (as display state don't hold)
   for (uint8_t t = 0; t < NUM_REDRAW_EACH_FRAME; ++t)
   {
     for (uint8_t i = 0; i < NUM_LED_MATRICES; ++i)
@@ -143,7 +133,7 @@ void loop()
                                                         (i * ONE_MATRIX_FRAME_SIZE)];
       for (uint8_t j = 0; j < NUM_ROWS_PER_MATRIX; ++j)
       {
-        digitalWrite(LEDMATRIX_SELECT_PINS[i], LOW);  // Select a slave LED matrix
+        digitalWrite(LED_MATRIX_SELECT_PINS[i], LOW);  // Select a slave LED matrix
 
         for (uint8_t k = 0; k < NUM_COLORS_PER_ROW_DOT; ++k)
         {
@@ -151,19 +141,19 @@ void loop()
         }
         SPI.transfer(0x01 << j);  // Send row index for current LED matrix
 
-        digitalWrite(LEDMATRIX_SELECT_PINS[i], HIGH); // Deselect the selected LED matrix
+        digitalWrite(LED_MATRIX_SELECT_PINS[i], HIGH); // Deselect the selected LED matrix
       }
       
       // Explicitly need to turn off the last row of LEDs in the matrix
       // before displaying next frame so that unwanted lingering previous
       // data for last row does not remain.
-      digitalWrite(LEDMATRIX_SELECT_PINS[i], LOW);
+      digitalWrite(LED_MATRIX_SELECT_PINS[i], LOW);
       for (uint8_t k = 0; k < NUM_COLORS_PER_ROW_DOT; ++k)
       {
         SPI.transfer(LED_ROW_OFF);
       }
       SPI.transfer(0x01 << (NUM_ROWS_PER_MATRIX - 1));
-      digitalWrite(LEDMATRIX_SELECT_PINS[i], HIGH);
+      digitalWrite(LED_MATRIX_SELECT_PINS[i], HIGH);
     }
   }
   
@@ -171,70 +161,42 @@ void loop()
   sprintf_P(g_pStringBuffer, (PGM_P)F("INFO: 1 frame took %i ms."), (millis() - start_time));
   SSerial.println(g_pStringBuffer);
   #endif
+
+  // Increment current frame index pointer
+  g_CurrentFrameIndex = (g_CurrentFrameIndex + 1) % TOTAL_FRAMES;
   
-  // Check if there is data available in Serial port from host
+  // Check if there is frame data (also could be reset command) available in Serial port from host.
+  // If so, read one frame and write to next frame position.
   if (SSerial.available())
   {
-    uint8_t CurrentFrameIndex = 0;
-    
-    do
+    // Read a frame of data and put it in position of next frame
+    byte *pBuffer = &g_pFrameBuffer[g_CurrentFrameIndex * ONE_FRAME_SIZE];
+    size_t bytesRead = SSerial.readBytes(pBuffer, ONE_FRAME_SIZE);
+    if (bytesRead == ONE_FRAME_SIZE)
     {
-      // Check if max frame memory has exceeded
-      if (CurrentFrameIndex == MAX_FRAMES)
-      {
-        SSerial.println(F("ERROR: Too many frames given!"));
-        CurrentFrameIndex = 0;
-      }
+      SSerial.println(F("OK: Received a good frame."));
+    }
+    else if (bytesRead == strlen(RESET_COMMAND) && 
+        strncmp((const char *)pBuffer, RESET_COMMAND, strlen(RESET_COMMAND)) == 0)
+    {
+      // Host has asked us to reset
+      SSerial.println(F("INFO: Resetting..."));
       
-      // Read a frame of data
-      byte *pBuffer = &g_pFrameBuffer[CurrentFrameIndex * ONE_FRAME_SIZE];
-      size_t bytesRead = SSerial.readBytes(pBuffer, ONE_FRAME_SIZE);
-      if (bytesRead == ONE_FRAME_SIZE)
-      {
-        SSerial.println(F("OK: Received a good frame."));
-      }
-      else if (bytesRead == strlen(RESET_COMMAND) && 
-          strncmp((const char *)pBuffer, RESET_COMMAND, strlen(RESET_COMMAND)) == 0)
-      {
-        // Host has asked us to reset
-        SSerial.println(F("INFO: Resetting..."));
-        
-        // NOTE: We use watchdog timer to reset the system
-        wdt_enable(WDTO_15MS);
-        while (true) {} // Let the watchdog timer fire
-      }
-      else
-      {
-        ClearSerialReceiveBuffer();
-        sprintf_P(g_pStringBuffer,
-                  (PGM_P)F("ERROR: Incorrect sized frame received! Expected %i but got %i bytes instead."),
-                  (int)bytesRead,
-                  (int)ONE_FRAME_SIZE);
-        SSerial.println(g_pStringBuffer);
-        
-        // We revert to default pattern
-        fillFrameBufferWithDefaultPattern();
-        return;
-      }
+      // NOTE: We use watchdog timer to reset the system
+      wdt_enable(WDTO_15MS);
+      while (true) {} // Let the watchdog timer fire
+    }
+    else
+    {
+      ClearSerialReceiveBuffer();
+      sprintf_P(g_pStringBuffer,
+                (PGM_P)F("ERROR: Incorrect sized frame received! Expected %i but got %i bytes instead."),
+                (int)bytesRead,
+                (int)ONE_FRAME_SIZE);
+      SSerial.println(g_pStringBuffer);
       
-      // Increment frame index to input new data for new frame, if it does arrive
-      ++CurrentFrameIndex;
-      
-      // Wait to see if more data arrives
-      delay(MILLIS_REQUIRED_PER_FRAME);
-    } while (SSerial.available());
-
-    SSerial.println(F("INFO: Receive complete."));
-    
-    // Set new size of frame buffer
-    g_FrameBufferSize = CurrentFrameIndex * ONE_FRAME_SIZE;
-  
-    // Reset current frame index pointer to first frame
-    g_CurrentFrameIndex = 0;
-  }
-  else
-  {
-    // Increment current frame index pointer
-    g_CurrentFrameIndex = (g_CurrentFrameIndex + 1) % int(g_FrameBufferSize / ONE_FRAME_SIZE);
+      // We revert to default pattern
+      fillFrameBufferWithDefaultPattern();
+    }
   }
 }
