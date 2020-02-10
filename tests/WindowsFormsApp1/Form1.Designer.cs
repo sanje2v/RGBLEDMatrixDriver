8756900fca63054ddd7677043087ae4999650291
Sanjeev <swtbase@gmail.com>
2020-02-10 13:07:14 +1100

    Added C# Windows Service whose use is to be further investigated. And a C# projects under tests/ so that we can compare python program speed to C# one.

---
+++ tests/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_serialoutput = new System.Windows.Forms.TextBox();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.timer_gui_dispatcher = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txt_serialoutput
+            // 
+            this.txt_serialoutput.Location = new System.Drawing.Point(13, 13);
+            this.txt_serialoutput.Multiline = true;
+            this.txt_serialoutput.Name = "txt_serialoutput";
+            this.txt_serialoutput.ReadOnly = true;
+            this.txt_serialoutput.Size = new System.Drawing.Size(523, 348);
+            this.txt_serialoutput.TabIndex = 0;
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(13, 368);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(0, 13);
+            this.lbl_status.TabIndex = 1;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(551, 11);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(88, 23);
+            this.btn_connect.TabIndex = 2;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Enabled = false;
+            this.btn_reset.Location = new System.Drawing.Point(551, 40);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(88, 23);
+            this.btn_reset.TabIndex = 3;
+            this.btn_reset.Text = "Send reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // timer_gui_dispatcher
+            // 
+            this.timer_gui_dispatcher.Interval = 200;
+            this.timer_gui_dispatcher.Tick += new System.EventHandler(this.timer_gui_dispatcher_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 400);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.txt_serialoutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_serialoutput;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Timer timer_gui_dispatcher;
+    }
+}
+
---
+++ tests/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static int NUM_FRAMES = 12;
+        private static int ONE_FRAME_SIZE = 96;
+        private static int NUM_COLORS_PER_DOT = 3;
+        private static string CONTROLLER_INITIALIZED_MESSAGE = "INITIALIZED";
+        private static string CONTROLLER_RESET_COMMAND = "RESET";
+
+        private List<byte> FRAMES;
+        private Thread thread_worker;
+        private ManualResetEventSlim thread_worker_signal;
+        private bool isControllerInitialized;
+        private Queue<Action> gui_dispatcher_queue;
+
+
+        private static void thread_worker_func(object _Form1)
+        {
+            string getNextMessageAndWriteOut(Form1 __this, SerialPort serialToController)
+            {
+                string message = String.Copy(serialToController.ReadLine());
+                __this.gui_dispatcher_queue.Enqueue(() => __this.txt_serialoutput.AppendText(message));
+
+                return message.TrimEnd();
+            }
+
+            Form1 _this = (Form1)_Form1;
+
+            for (int i = 0; i < NUM_FRAMES; ++i)
+            {
+                for (int j = 0; j < ONE_FRAME_SIZE; j += NUM_COLORS_PER_DOT)
+                {
+                    if (i < 3)
+                    {
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 0] = 0x00;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 2] = 0xFF;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 1] = 0xFF;
+                    }
+                    else if (i < 6)
+                    {
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 0] = 0xFF;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 2] = 0x00;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 1] = 0xFF;
+                    }
+                    else if (i < 9)
+                    {
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 0] = 0xFF;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 2] = 0xFF;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 1] = 0x00;
+                    }
+                    else
+                    {
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 0] = 0xFF;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 2] = 0x00;
+                        _this.FRAMES[i * ONE_FRAME_SIZE + j + 1] = 0x00;
+                    }
+                }
+            }
+
+            Debug.Assert(_this.FRAMES.Count % ONE_FRAME_SIZE == 0);
+
+            try
+            {
+                using (var serialToController = new SerialPort("COM1", 52700, Parity.None, 8, StopBits.One))
+                {
+                    serialToController.NewLine = "\r\n";
+
+                    serialToController.DiscardInBuffer();
+                    serialToController.DiscardOutBuffer();
+
+                    // Ask controller to reset
+                    serialToController.WriteLine(CONTROLLER_RESET_COMMAND);
+                    _this.gui_dispatcher_queue.Enqueue(() => _this.lbl_status.Text = "Reset sent. Waiting for initializing message from controller.");
+
+                    // Set the index in FRAME of next frame
+                    var next_frame_start_position = 0;
+
+                    while (!_this.thread_worker_signal.IsSet)
+                    {
+                        if (serialToController.BytesToRead > 0)
+                        {
+                            var message = getNextMessageAndWriteOut(_this, serialToController);
+
+                            if (!_this.isControllerInitialized && message.Equals(CONTROLLER_INITIALIZED_MESSAGE))
+                            {
+                                _this.isControllerInitialized = true;
+                                _this.gui_dispatcher_queue.Enqueue(() => _this.lbl_status.Text = "Controller initialized. Ready.");
+                            }
+                            else if (_this.isControllerInitialized && message.StartsWith("SYNC"))
+                            {
+                                serialToController.Write(_this.FRAMES.ToArray(), next_frame_start_position, ONE_FRAME_SIZE);
+                                next_frame_start_position = (next_frame_start_position + ONE_FRAME_SIZE) % _this.FRAMES.Count;
+
+                                bool hasErrorOccurred = true;
+                                while (true)
+                                {
+                                    message = getNextMessageAndWriteOut(_this, serialToController);
+                                    if (message.StartsWith("OK"))
+                                        break;
+                                    else if (message.StartsWith("ERROR"))
+                                    {
+                                        _this.gui_dispatcher_queue.Enqueue(() => _this.lbl_status.Text = "Error occurred!");
+                                        _this.isControllerInitialized = false;
+                                        hasErrorOccurred = true;
+                                        break;
+                                    }
+                                }
+
+                                if (hasErrorOccurred)
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var ex_str = "Exception occurred: " + ex.Message.ToString();
+                _this.gui_dispatcher_queue.Enqueue(() => _this.lbl_status.Text = ex_str);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FRAMES = new List<byte>(Form1.NUM_FRAMES * Form1.ONE_FRAME_SIZE);
+            for (int i = 0; i < (Form1.NUM_FRAMES * Form1.ONE_FRAME_SIZE); ++i)
+                this.FRAMES.Add(0xFF);
+            this.thread_worker = new Thread(thread_worker_func);
+            this.thread_worker_signal = new ManualResetEventSlim(false);
+            this.isControllerInitialized = false;
+            this.gui_dispatcher_queue = new Queue<Action>();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            btn_connect.Enabled = false;
+            btn_reset.Enabled = true;
+            timer_gui_dispatcher.Enabled = true;
+
+            this.thread_worker.Start(this);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.thread_worker_signal.Set();
+            this.thread_worker.Join(2000);
+        }
+
+        private void timer_gui_dispatcher_Tick(object sender, EventArgs e)
+        {
+            while (this.gui_dispatcher_queue.Count > 0)
+            {
+                this.gui_dispatcher_queue.Dequeue()();
+            }
+        }
+    }
+}
