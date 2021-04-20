ed386181cc98c9953a0d4f80c5af8bb1178aaf29
Sanjeev <swtbase@yahoo.com>
2021-04-21 00:59:17 +1000

    Added GPU temperature but CPU temperature is unimplement due to difficulty getting proper value in Windows.

--- RGBLEDMatrixDriver/funcs/cpugpu_usage/cpugpu_usage.py
+++ RGBLEDMatrixDriver/funcs/cpugpu_usage/cpugpu_usage.py
@@ -11,6 +11,11 @@ from enums import IntervalEnum
 class cpugpu_usage:
     MAX_LINE_LENGTH_PX = 15
     LINE_WIDTH = 3
+    CPU_TEXT_COLOR_VALUE = [255]*3
+    GPU_TEXT_COLOR_VALUE = [254]*3
+    GREEN_COLOR = [0x00, 0xFF, 0x00]
+    YELLOW_COLOR = [0xFF, 0xFF, 0x00]
+    RED_COLOR = [0xFF, 0x00, 0x00]
 
 
     @staticmethod
@@ -24,16 +29,19 @@ class cpugpu_usage:
         # CAUTION: Need to call usage functions at least once
         #          before calling it in 'get_frame()'
         self.cpu_usage_percent = psutil.cpu_percent()
+        self.cpu_temperature = 0.0
 
         nvml.nvmlInit()  # CAUTION: Must be called before any other nvml functions
+        assert nvml.nvmlDeviceGetCount() > 0, "No NVIDIA GPU found."
         self.selected_gpu = nvml.nvmlDeviceGetHandleByIndex(0)
         self.gpu_usage_percent = nvml.nvmlDeviceGetUtilizationRates(self.selected_gpu).gpu
+        self.gpu_temperature = nvml.nvmlDeviceGetTemperature(self.selected_gpu, nvml.NVML_TEMPERATURE_GPU)
 
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
-        pass
+        nvml.nvmlShutdown()
 
     def get_interval(self):
         return IntervalEnum.MSECS_500
@@ -49,16 +57,31 @@ class cpugpu_usage:
         # Draw progress bar for each usage
         def getUsageLineColor(usage):
             if usage < 30.:
-                return [0x00, 0xFF, 0x00]
+                return self.GREEN_COLOR
             elif usage < 70.:
-                return [0xFF, 0xFF, 0x00]
+                return self.YELLOW_COLOR
             else:
-                return [0xFF, 0x00, 0x00]
+                return self.RED_COLOR
 
         cpu_line_length_px = int(self.cpu_usage_percent / 100. * self.MAX_LINE_LENGTH_PX)
         gpu_line_length_px = int(self.gpu_usage_percent / 100. * self.MAX_LINE_LENGTH_PX)
 
         frame[1:(cpu_line_length_px + 1), 0:self.LINE_WIDTH, :] = getUsageLineColor(self.cpu_usage_percent)
         frame[18:(gpu_line_length_px + 18), 0:self.LINE_WIDTH, :] = getUsageLineColor(self.gpu_usage_percent)
+
+        # Color CPU and GPU text according to temperature
+        def getCPUGPUTextColor(temp):
+            if temp < 40.:
+                return self.GREEN_COLOR
+            elif temp < 70.:
+                return self.YELLOW_COLOR
+            else:
+                return self.RED_COLOR
+
+        self.cpu_temperature = 0.0
+        self.gpu_temperature = nvml.nvmlDeviceGetTemperature(self.selected_gpu, nvml.NVML_TEMPERATURE_GPU)
+
+        IMAGE_CHANNEL_AXIS = -1 # Last axis
+        frame[(frame==self.GPU_TEXT_COLOR_VALUE).all(axis=IMAGE_CHANNEL_AXIS)] = getCPUGPUTextColor(self.gpu_temperature)
 
         return frame.flatten()
